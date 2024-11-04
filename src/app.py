f5bfdfb2cb7dc8ce7d2708f708930b1ddad19834
Ramirez Vazquez Sofia Elizabeth <22030027@upgto.edu.mx>
2024-11-03 22:40:08 -0600

    Trabajo hecho

--- src/app.py
+++ src/app.py
@@ -1,31 +1,43 @@
-from flask import Flask, render_template
-import plotly.express as px
+from flask import Flask, request, render_template
 import pandas as pd
+import plotly.express as px
+import plotly.io as pio
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html')  # Renderiza index4.html desde la carpeta templates
+def upload_form():
+    return render_template('index.html')
 
-@app.route('/grafica')
-def grafica():
-    # Cargar datos y crear la figura
-    df = pd.read_csv('data/test_1.csv')
-    conteo_colores = df['color'].value_counts()
+@app.route('/upload', methods=['POST'])
+
+def upload_file():
+    if 'file' not in request.files:
+        return 'No file part'
+
+    file = request.files['file']
+
+    if file.filename == '':
+        return 'No selected file'  
+
+    if file and file.filename.endswith('.csv'):
+
+        df = pd.read_csv(file)
+
+        conteo_colores = df['color'].value_counts()
+
+        fig = px.pie(
+            values = conteo_colores,
+            names = conteo_colores.index,
+            title = 'Aquí va un Title',
+            color = conteo_colores.index,
+            color_discrete_sequence = ['red', 'green'],   
+        )
+
+        graph_html = pio.to_html(fig, full_html=False)
+        return render_template('graph.html', graph_html=graph_html)
     
-    fig = px.pie(
-        values=conteo_colores,
-        names=conteo_colores.index,
-        title='',
-        color=conteo_colores.index,
-        color_discrete_sequence=['red', 'green'],
-    )
-    
-    # Guardar la figura como un objeto HTML
-    graph_html = fig.to_html(full_html=False)
-    
-    return graph_html  # Retorna el HTML de la gráfica
+    return 'El archivo debe ser un archivo CSV (.csv)'
 
 if __name__ == '__main__':
     app.run(debug=True)
